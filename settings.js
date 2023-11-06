55b9eab46fb1168ca6c60efd52093b93d09b55c2
manjisama1 <111729787+manjisama1@users.noreply.github.com>
2023-11-06 23:10:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,20 +19,20 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['919862717507']
+global.premium = ['919862717507']
+global.ownernomer = 'Manji Sama'
+global.ownername = '919862717507'
+global.botname = 'Manji-BOT'
+global.footer = 'manjisama'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
 global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.author = '—͟͞͞🅜𝗮𝗻𝗷𝗶 𝗦𝗮𝗺𝗮𓏲'
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
